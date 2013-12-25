816dbaf8a0584f0c7ac3d282a8574e7bfb4bf41c
Shenjiaqi <Shenjiaqi@whatbecome@163.com>
2013-12-25 20:58:00 +0800

    221 not finished yet

---
+++ 221/221.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <fstream>
+#include <cassert>
+#include <set>
+#include <queue>
+#include <iostream>
+#include <utility>
+#include <stack>
+#include <complex>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <list>
+#include <functional>
+#include <cctype>
+using namespace std;
+typedef long double ld;
+typedef long long ll;
+typedef pair<int,int> ppi;
+typedef pair<ll,ll> ppl;
+typedef pair<ld,ld> ppld;
+#define PB push_back
+#define FIR first
+#define SEC second
+#define MP make_pair
+#define FOR(a,b,c) for(int a=(b);a<(c);++a)
+#define FR(a,b) for(typeof(b.begin()) a=b.begin();a!=b.end();++a)
+int n,k;
+vector<int> a,b;
+int main()
+{
+  scanf("%d%d",&n,&k);
+  for(int i=1;i<=n;i+=2)
+    {
+      a.PB(i);
+      if(i+1<=n)
+	b.PB(i+1);
+    }
+}
