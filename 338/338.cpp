5da945e90e501ba0c6e7a0be613aaeab3ff35c1a
Shenjiaqi <Shenjiaqi@whatbecome@163.com>
2013-12-26 11:41:40 +0800

    388 coding

---
+++ 338/338.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <fstream>
+#include <cassert>
+#include <set>
+#include <queue>
+#include <iostream>
+#include <utility>
+#include <stack>
+#include <complex>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <list>
+#include <functional>
+#include <cctype>
+using namespace std;
+typedef long double ld;
+typedef long long ll;
+typedef pair<int,int> ppi;
+typedef pair<ll,ll> ppl;
+typedef pair<ld,ld> ppld;
+#define PB push_back
+#define FIR first
+#define SEC second
+#define MP make_pair
+#define FOR(a,b,c) for(int a=(b);a<(c);++a)
+#define FR(a,b) for(typeof(b.begin()) a=b.begin();a!=b.end();++a)
+ld pv[27];
+int main()
+{
+  pv[0]=1;
+  for(int i=1;i<=26;++i)
+    {
+      pv[i]=0;
+      for(int j=max(0,i-6);j<i;++j)
+	pv[i]+=pv[j];
+      pv[i]/=6.0;
+    }
+  
+  return 0;
+}
