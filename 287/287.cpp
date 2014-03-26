bc860a4be15e015912cb1dc379b8070dfc143e06
shenjiaqi <1029393956@qq.com>
2014-03-26 10:21:41 +0800

    287 got ac

--- 287/287.cpp
+++ 287/287.cpp
@@ -29,15 +29,30 @@ typedef pair<double,double> ppd;
 #define SEC second
 #define FOR(a,b,c) for(int a=(b);a<(c);++a)
 #define FR(a,b) for(typeof(b.begin()) a=b.begin();a!=b.end();++a)
+const int M = 1e6 + 7;
 ll q;
 int c;
+ll dp[M];
+int addMod( int a, int b)
+{
+  return ( a + b ) % M;
+}
+int subMod( int a, int b)
+{
+  return addMod( (a - b) % M, M );
+}
 int main()
 {
   cin >> q >> c;
-  int ans(0);
-  for(; ; )
+  for( int i = c; ; ++i )
     {
-      
+      if( dp[ addMod( i, 0 ) ] >= q )
+	{
+	  cout << i;
+	  break;
+	}
+      dp[ addMod( i, 1 ) ] = 
+	dp[ subMod( i, c) ] + 1 + dp[ addMod( i, 0 ) ];
     }
   return 0;
 }
